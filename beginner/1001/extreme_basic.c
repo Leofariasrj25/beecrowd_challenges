6d6153f7fa955072533e9c64cca496f20e92beff
lfarias- (Leonardo Farias) <leofariasrj25@gmail.com>
2022-09-21 00:46:38 -0300

    challenge 1001

---
+++ beginner/1001/extreme_basic.c
@@ -0,0 +1,16 @@
+// challenge 1001
+// objective read two variables and perform the sum of these two. store the result on a variable
+
+#include <stdio.h>
+
+int	main(int argc, char *argv[])
+{
+	int	a;
+	int	b;
+	int	x;
+
+	scanf("%d %d", &a, &b);
+	x = a + b;
+	printf("X = %d\n", x);
+	return (0);
+}
