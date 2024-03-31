488c2ab53f93a498cbe14d6afa35c0c0903da5dc
Leonardo Farias <leofariasrj25@gmail.com>
2024-03-30 23:42:21 -0300

    ex1004 - simple product (java ver)

---
+++ beginner/1004/SimpleProduct.java
@@ -0,0 +1,13 @@
+// challenge link: https://judge.beecrowd.com/en/problems/view/1004
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SimpleProduct {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		System.out.println("PROD = " + (a * b));
+	}
+}
