c2865ca3ddc7bb532b5c2646313a1ec66fa8e95f
Leonardo Farias <leofariasrj25@gmail.com>
2024-08-02 21:28:22 -0300

    ex1114 - fixed password (java ver)

---
+++ beginner/1114 - fixed password/FixedPassword.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class FixedPassword {
+	public static void main(String[] args) throws IOException {
+		Scanner reader = new Scanner(System.in);
+		int password = 2002;
+
+		while (true) {
+			int input = reader.nextInt();
+
+			if (input == password) {
+				System.out.println("Acesso Permitido");
+				break;
+			}
+
+			System.out.println("Senha Invalida");
+		}
+	}
+}
